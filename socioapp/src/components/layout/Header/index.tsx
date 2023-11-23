575f32b9e2914831d73a077936df4279a22d2753
Arthur de Araujo Neves <123080438+ArthurDeAraujoNeves3@users.noreply.github.com>
2023-11-23 10:54:03 -0300

    Revert "Revert "cabeçalho re-organizado com ajuda de tututs e inicio do hook para troca de tema""
    This reverts commit bb17ea50b8ccd84564f5c53ae78cc63ca9281adf.

--- socioapp/src/components/layout/Header/index.tsx
+++ socioapp/src/components/layout/Header/index.tsx
@@ -1,77 +1,228 @@
 'use client'
 import Image from "next/image";
-import Link from "next/link";
 import Logo from "../../../assets/img/AfroLogo.png";
-import menuHamburguer from "../../../assets/icons/menuHamburguer.png"
-import doubleRight from "../../../assets/icons/doubleRight.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { BsPeople } from "react-icons/bs";
+import { BsBook } from "react-icons/bs";
+import { MdDiversity1 } from "react-icons/md";
+import { TbWorldPlus } from "react-icons/tb";
+import { MdLightbulbOutline } from "react-icons/md";
+import { BsMegaphone } from "react-icons/bs";
+import { MdOutlineCastForEducation } from "react-icons/md";
+import { RiContactsLine } from "react-icons/ri";
+import { FaHeadSideVirus } from "react-icons/fa";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { useState } from "react";
 export default function Header() {
-    const[menuAtivado, setMenuAtivado] = useState(false);/* */
+
+    const[menuAtivado, setMenuAtivado] = useState(false);
+    const[dark, setDark] = useState(false);
+
     function abrirMenu() {
 
-        if (menuAtivado == false) {setMenuAtivado(true);}
+        if (menuAtivado == false) {
+            setMenuAtivado(true);
+        }
+        else {
+            setMenuAtivado(false);
+        }
 
-        else {setMenuAtivado(false);}
+    }
 
-    }/*estrutura de condição para menu hamburguer*/
-    const[botaoAtivado, setBotaoAtivado] = useState(false);/* */
-    function virarBotao() {
+    function fecharMenu(){
 
-        if (botaoAtivado == false) {setBotaoAtivado(true);}
+        if (menuAtivado == false) {
+            setMenuAtivado(false);
+        }
+        else {
+            setMenuAtivado(false);
+        }
 
-        else {setBotaoAtivado(false);}
+    }/estrutura de condição para menu hamburguer/
 
-    }/*estrutura de condição para menu hamburguer*/
+    function trocarTema() {
+
+        if (dark == false) {
+
+            setDark(true);
+        }
+
+        else {
+
+            setDark(false);
+        }
+
+    }/estrutura de condição para menu hamburguer/
+
     return(
-        <div className={`h-screen w-[400px] bg-white-facebook pt-2 lg:w-[20px] lg:bg-white`}>
-            <div className="justify-between gap-5 flex flex-col items-center h-full">
-                <div className="flex lg:hidden">
-                    <Image src={Logo} alt="Logo" className="w-[250px]" />
-                </div>
 
-                <div className="">
-                    <div className="hidden lg:flex absolute">
-                        <button className="w-[20px] absolute" onClick={abrirMenu}>
-                            <Image src={menuHamburguer} alt="Menu Hamburguer" width={20} height={20} />
-                        </button>
+        <>
+
+            <RxHamburgerMenu onClick={abrirMenu}  className={`absolute z-10 hidden text-2xl text-black-bg mt-2 ml-2 cursor-pointer lg:flex ${menuAtivado? 'lg:hidden' : 'flex'}`}/>
+            
+            <div className={`h-screen w-[500px] bg-white-facebook pt-2 duration-[0.2s] lg:ml-[-500px]`}>
+
+                <div className="flex flex-col items-center justify-between gap-5 p-5 h-full ">
+
+                    <Image src={Logo} className="max-w-full" draggable="false" loading="eager" alt="Logo do projeto"/> 
+
+                    <div className="flex flex-col items-start">
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsPeople className="text-2xl"/> 
+                            <a href="#quemSao" className="text-3xl font-bold">Quem são</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsBook className="text-2xl" /> 
+                            <a href="#legislacao" className="text-3xl font-bold">Legislação</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdDiversity1 className="text-2xl" />
+                            <a href="#diversidade" className="text-3xl font-bold">Diversidade</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <TbWorldPlus className="text-2xl" />
+                            <a href="#cultura" className="text-3xl font-bold">Cultura</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdLightbulbOutline className="text-2xl" />
+                            <a href="#desafios" className="text-3xl font-bold">Desafios</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsMegaphone className="text-2xl" />
+                            <a href="#acoes" className="text-3xl font-bold">Ações</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdOutlineCastForEducation className="text-2xl" />
+                            <a href="#educacao" className="text-3xl font-bold">Educação</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <RiContactsLine className="text-2xl" />
+                            <a href="#parcerias" className="text-3xl font-bold">Parcerias</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <FaHeadSideVirus className="text-2xl" />
+                            <a href="#perspectivas" className="text-3xl font-bold">Perspectivas</a>
+
+                        </div> 
+
+                    </div> 
+
+                    <div onClick={ trocarTema } className={` w-16 p-1 rounded-full cursor-pointer ${dark? 'bg-gray-text' : 'bg-black-2'}`}>
+                        <div onClick={trocarTema} className={`${dark ? "ml-[64%] bg-black" : "bg-white"} w-5 h-5 rounded-full duration-[0.2s] cursor-pointer`}></div>
                     </div>
                     
-                    <div className={`${menuAtivado? 'duration-[0.8s] lg:ml[1px] lg:absolute top-0 left-0 ':'lg:ml-[-500px]'} flex flex-col text-2xl font-bold lg:bg-white-facebook md:w-[110px] lg:h-[2000px] text-gray-text gap-3 h-full lg:w-[165px] lg:text-xl lg:items-center`}>
-                        <div className="top-5 hidden lg:flex absolute">
-                            <Image src={Logo} alt="Logo" className="w-[250px] lg:w-[120px]" />
-                        </div>
-                        <div className="flex flex-col gap-4 lg:gap-10 lg:mt-[80%] ml-10">
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10 ">Sobre</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Legislação</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Cultura</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Desafios</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Ações</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Educação</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Parcerias</a>
-                            <a href="#QuemSao" className="hover:text-black transition-all duration-[0.2s] mr-10">Perspectivas</a>
-                        </div>
-                        <div className="hidden lg:flex bottom-3 items-center justify-center mr-[40px] mb-3 lg:mr-[33px] mt-[200px] "> 
-                            <label htmlFor="" className="">
-                                <button className={`absolute w-[40px] h-[30] bg-gray-button rounded-2xl flex items-center `} onClick={virarBotao}>
-                                    <span className={`w-[16px] h-[16px] bg-white rounded-full justify-start ml-[0,8px] ${botaoAtivado? 'ml-[23px] duration-[0.8s]' : 'ml=[0,8px] duration-[0.8s]'}`}></span>
-                                </button>
-                            </label>
-                        </div>
-                    </div>
                 </div>
-                <div className="flex items-center justify-center mr-[40px] mb-6 lg:mr-[-5px] lg:hidden"> 
-                    <label htmlFor="">
-                        <button className={`absolute w-[40px] h-[30] bg-gray-button rounded-2xl flex items-center`} onClick={virarBotao}>
-                            <span className={`w-[16px] h-[16px] bg-white rounded-full justify-start ml-[0,8px] ${botaoAtivado? 'ml-[23px] duration-[0.8s]' : 'ml=[0,8px] duration-[0.8s]'}`}></span>
-                        </button>
-                    </label>
-                </div>
-                
+
             </div>
-            <body>
-                
-            </body>
-        </div>
-        
-    );
+
+            <div onClick={fecharMenu} className={`hidden lg:block duration-[0.2s] ${menuAtivado ? "bg-white-facebook/60 backdrop-blur-sm" : "bg-white-facebook/0 backdrop-blur-0"} fixed w-screen h-screen`}></div>
+
+            <div className={`hidden lg:flex fixed h-screen w-[250px] max-w-full bg-white-facebook duration-[0.2s] ${menuAtivado ? "ml-0" : "ml-[-400px]"}`}>
+
+                <div className="flex flex-col items-center justify-between gap-5 p-5 h-full">
+
+                    <Image src={Logo} className="max-w-full" draggable="false" loading="eager" alt="Logo do projeto"/>
+
+                    <div className="flex flex-col items-start">
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsPeople className="text-2xl"/>
+                            <a href="#quemSao" className="text-2xl font-bold">Quem são</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsBook className="text-2xl" />  
+                            <a href="#legislacao" className="text-2xl font-bold">Legislação</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdDiversity1 className="text-2xl" />
+                            <a href="#diversidade" className="text-2xl font-bold">Diversidade</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <TbWorldPlus className="text-2xl" />
+                            <a href="#cultura" className="text-2xl font-bold">Cultura</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdLightbulbOutline className="text-2xl" />
+                            <a href="#desafios" className="text-2xl font-bold">Desafios</a>
+
+                        </div>
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <BsMegaphone className="text-2xl" />
+                            <a href="#acoes" className="text-2xl font-bold">Ações</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <MdOutlineCastForEducation className="text-2xl" /> 
+                            <a href="#educacao" className="text-2xl font-bold">Educação</a>
+
+                        </div> 
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <RiContactsLine className="text-2xl" />
+                            <a href="#parcerias" className="text-2xl font-bold">Parcerias</a>
+
+                        </div>
+
+                        <div className="flex items-center gap-3 rounded-lg cursor-pointer py-3 px-3 duration-[0.2s] hover:bg-gray-text">
+
+                            <FaHeadSideVirus className="text-2xl" />
+                            <a href="#perspectivas" className="text-2xl font-bold">Perspectivas</a>
+
+                        </div>
+
+                    </div> 
+
+                    <div onClick={ trocarTema } className={` w-16 p-1 rounded-full cursor-pointer ${dark? 'bg-gray-text' : 'bg-black-2'}`}>
+                        <div onClick={trocarTema} className={`${dark ? "ml-[64%] bg-black" : "bg-white"} w-5 h-5 rounded-full duration-[0.2s] cursor-pointer`}></div>
+                    </div>
+
+                </div> 
+            </div> 
+
+        </>
+    );
 };

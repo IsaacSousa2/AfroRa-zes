859c45f8c021287901b3b90106934d6f61b17d3c
cicero.isaac <ciceroisaacsc@gmail.com>
2023-11-25 09:28:40 -0300

    Correção de bugs de header
    Correção de link

--- socioapp/src/components/Sections/Acoes/index.tsx
+++ socioapp/src/components/Sections/Acoes/index.tsx
@@ -6,7 +6,7 @@ export default function Acoes() {
 
     return(
 
-        <div id="acoes" className="flex flex-col p-10 gap-5">
+        <div id="#acoes" className="flex flex-col p-10 gap-5">
 
             <Section title="Ações de resistência e empoderamento" text="As comunidades quilombolas se destacam por suas ações resilientes de resistência e empoderamento. Projetos sociais, iniciativas culturais e a promoção da igualdade de gênero são evidências tangíveis do compromisso contínuo dessas comunidades com a melhoria de suas condições de vida." image={true} src={PovoQuilomba} desc="Exposição feita para relembrar a resistência e memória dos povos quilombolas" alt="Foto do povo quilomba"/>
             <LinkInfo href="https://www.correiobraziliense.com.br/euestudante/cultura/2023/05/5098292-exposicao-retrata-resistencia-e-memoria-de-quilombos-do-jequitinhonha.html" text="Saiba mais sobre a exposição, clique aqui."/>
--- socioapp/src/components/Sections/Legislacao/index.tsx
+++ socioapp/src/components/Sections/Legislacao/index.tsx
@@ -10,7 +10,7 @@ export default function Legislacao() {
 
             <Section title="Legislação e reconhecimento" text={`A legislação brasileira, com destaque para a Constituição de 1988, reconhece a importância dos quilombos, garantindo-lhes direitos territoriais e culturais. O reconhecimento legal é um marco significativo na promoção da justiça social e na preservação das raízes culturais dessas comunidades.`} image={true} src={PovoQuilomba} desc="Crianças quilombolas escrevendo" alt="Foto de duas crianças escrevendo"/>
 
-            <LinkInfo href="#" text="Clique aqui para saber mais sobre a legislação de 1988."/>
+            <LinkInfo href="https://www.camara.leg.br/noticias/899296-projeto-preve-regras-para-regularizacao-de-terras-quilombolas/#:~:text=O%20Projeto%20de%20Lei%201942,foi%20promulgada%20a%20atual%20Constitui%C3%A7%C3%A3o)." text="Clique aqui para saber mais sobre a legislação de 1988."/>
 
         </div>
     );
